92c237d3664ca18d5ff502fe028b32bcdb2f6717
GodOfCoding1 <madhavrajeev303@gmail.com>
2024-11-02 22:25:30 +0530

    added dynamic image scaling

--- app/home.tsx
+++ app/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   VStack,
@@ -10,6 +10,26 @@ import {
   Center,
 } from "native-base";
 import { Image, Dimensions } from "react-native";
+import axios from "axios";
+import { Buffer } from "buffer";
+import { ThemedText } from "@/components/ThemedText";
+
+interface DimensionsType {
+  width: number;
+  height: number;
+}
+
+const extractDimensions = (imageStr: string): Promise<DimensionsType> => {
+  return new Promise((resolve, reject) => {
+    const imageHelper = Image.getSize(
+      imageStr,
+      (width, height) => {
+        resolve({ width, height });
+      },
+      (error) => reject(error)
+    );
+  });
+};
 
 function Home() {
   const [masterPassword, setMasterPassword] = useState("");
@@ -29,13 +49,40 @@ function Home() {
   ];
 
   // Function to shuffle the images array
-  const getRandomImages = () => {
-    return images.sort(() => 0.5 - Math.random());
+  const getBase64Images = async () => {
+    const res = [];
+    for (let i = 0; i < images.length; i++) {
+      const response = await axios.get(images[i], {
+        responseType: "arraybuffer",
+      });
+
+      try {
+        const imageStr =
+          "data:image;base64," +
+          Buffer.from(response.data, "binary").toString("base64");
+        res.push({
+          image: imageStr,
+          dimensions: await extractDimensions(imageStr),
+        });
+      } catch (error) {
+        console.log("error occured when processing image buffer", error);
+      }
+    }
+    console.log("done processing images");
+
+    setImageData(res);
   };
+
+  const [imageData, setImageData] = useState<
+    { image: string; dimensions: DimensionsType }[]
+  >([]);
+
+  useEffect(() => {
+    getBase64Images();
+  }, []);
 
   // Get screen width for full-width image display
   const screenWidth = Dimensions.get("window").width;
-
   return (
     <Box flex={1} p={5} safeArea>
       <VStack space={4} alignItems="center">
@@ -50,19 +97,28 @@ function Home() {
         />
 
         {/* Display Random Images */}
-        <FlatList
-          data={getRandomImages()}
-          renderItem={({ item }) => (
-            <Box mb={4}>
-              <Image
-                source={{ uri: item }}
-                style={{ width: screenWidth }}
-                alt="random image"
-              />
-            </Box>
-          )}
-          width="100%"
-        />
+        {imageData.length > 0 ? (
+          <FlatList
+            data={imageData}
+            renderItem={({ item }) => (
+              <Box mb={4}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={{
+                    width: screenWidth,
+                    height:
+                      (item.dimensions.height / item.dimensions.width) *
+                      screenWidth,
+                  }}
+                  alt="random image"
+                />
+              </Box>
+            )}
+            width="100%"
+          />
+        ) : (
+          <ThemedText>waiting to decode image. please wait..</ThemedText>
+        )}
       </VStack>
     </Box>
   );
